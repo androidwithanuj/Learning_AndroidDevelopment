9ab61799d14e28239413b8fbbeb59a8868d24ed0
Anuj Singh <anujintech@gmail.com>
2026-09-11 15:37:21 +0530

    Add Kotlin collection examples
    Demonstrate arrays, lists, sets, and maps in the Module 02 collection lesson, including array iteration and list size output.

--- Kotlin/Courses/KotlinCourse-01/src/Module_02/collection.kt
+++ Kotlin/Courses/KotlinCourse-01/src/Module_02/collection.kt
@@ -1,6 +1,21 @@
 package Module_02
 
 fun main() {
-    
+    val numbers = arrayOf(1,2,3,4,5,6,7)
+    println(numbers.contentToString())
 
+    for (index in numbers.indices) {
+        println(numbers[index])
+    }
+
+    val list = listOf(1,2,3,4,5,6,7,8,9,10)
+    println(list)
+    println( "Size : " + list.size )
+
+    val set = setOf(1,2,3,4,5,5,6,6,7,8)
+    println(set)
+
+    // Maps
+    val map = mapOf(1 to "one", 2 to "two", 3 to "three")
+    println(map)
 }
